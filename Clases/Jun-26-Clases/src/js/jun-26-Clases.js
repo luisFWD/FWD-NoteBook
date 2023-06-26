51c7a27ad8edc2a0a5a74b2d124e1e085f896ae3
luisFWD <luisdaguilar@fwdcostarica.com>
2023-06-26 15:58:17 -0600

    [feat] [Javascript] Updates the class example with the car class and  book class

--- Clases/Jun-26-Clases/src/js/jun-26-Clases.js
+++ Clases/Jun-26-Clases/src/js/jun-26-Clases.js
@@ -9,9 +9,76 @@ var animal = {
 console.log("Propiedad 1 es :", animal.nombre);
 
 class MiAnimal {
-    nombre = ""
-    peso = 0
+    nombre = "";
+    peso = 0;
+
+    constructor(nombre, peso) {
+        this.nombre = nombre;
+        this.peso = peso;
+    }
 }
-var objAnimal = new MiAnimal();
+var objAnimal = new MiAnimal("Jirafa", 234);
+var elefante = new MiAnimal("Elefante", 500);
 
 console.log("Mi Animalito :", objAnimal.nombre, objAnimal.peso);
+console.log("Mi Animalito :", elefante.nombre, elefante.peso);
+
+
+class Auto {
+    marca = "volkswagen";
+    modelo = "ID4";
+    color = "red";
+
+    constructor(colorCliente) {
+        if (colorCliente) {
+            this.color = colorCliente;
+        }
+        else { //color por defecto
+            this.color = "red";
+        }
+    }
+    //metodos // funciones
+    imprimir() {
+        console.log("Hola soy un auto:", this.color);
+    }
+
+}
+//objeto o instancia
+var newCar = new Auto();
+var newCar2 = new Auto();
+var autoReserva = new Auto("Blue");
+
+console.log("Primer auto", newCar);
+console.log("Segundo auto", newCar2);
+console.log("Tercer auto", autoReserva);
+console.log(autoReserva.imprimir());
+
+
+
+
+class Libro {
+    titulo = "";
+    numeroDePaginas = 0;
+
+    constructor(tituloParam, paginasParam) {
+        this.titulo = tituloParam;
+        this.numeroDePaginas = paginasParam;
+    }
+
+    describirLibro() {
+        console.log(`Este libro es el ${this.titulo} 
+                     con paginas ${this.numeroDePaginas}`);
+    }
+
+    getTitulo() {
+        return this.titulo;
+    }
+
+}
+
+let libro1 = new Libro("Quijote", 1500);
+let libro2 = new Libro("Libro selva", 20);
+
+libro1.describirLibro();
+libro2.describirLibro();
+console.log(" titulo", libro1.getTitulo())
