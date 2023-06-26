235f4a5ad3121b2a74e7bc5e033fa016891a8bf7
luisFWD <luisdaguilar@fwdcostarica.com>
2023-06-26 15:28:48 -0600

    [feat][Javascript] Add Javascript project that explains the Clases

---
+++ Clases/Jun-26-Clases/src/js/jun-26-Clases.js
@@ -0,0 +1,17 @@
+
+var miLista = [];
+
+var animal = {
+    nombre: "Elefante",
+    peso: 2000
+}
+
+console.log("Propiedad 1 es :", animal.nombre);
+
+class MiAnimal {
+    nombre = ""
+    peso = 0
+}
+var objAnimal = new MiAnimal();
+
+console.log("Mi Animalito :", objAnimal.nombre, objAnimal.peso);
