c9d6179837fcc2358e7e78fa7c03a3e649b21bd9
luisFWD <luisdaguilar@fwdcostarica.com>
2023-07-06 12:45:58 -0600

    [feat][Javascript/html/css] Add Game Validations

--- Clases/ProyectoGato/src/js/logica.js
+++ Clases/ProyectoGato/src/js/logica.js
@@ -1,11 +1,11 @@
-
-
-
 // 1. Importaciones de módulos (si es necesario)
 // No se requieren importaciones en este ejemplo
 
 // 2. Declaración de variables
 var jugadorActual = "X";
+
+var turnosRestantes = 9;
+var ganadorJuego = false;
 
 var matrizJuego = [
     [{ celda: "celda-00", jugador: "" }, { celda: "celda-01", jugador: "" }, { celda: "celda-02", jugador: "" }],
@@ -15,20 +15,10 @@ var matrizJuego = [
 
 
 
-// F  C
-// 2 0
-// 1 1 
-// 0 2
-
-
-
 // 3. Definición de funciones
 function validarDiagonalA(jugador) {
-
     let ganador = false;
-
     for (let contador = 0; contador < matrizJuego.length; contador++) {
-
         if (matrizJuego[contador][contador].jugador === jugador) {
             ganador = true;
         } else {
@@ -40,20 +30,15 @@ function validarDiagonalA(jugador) {
 }
 
 function validarDiagonalB(jugador) {
-
     let contadorFila = 2;
-
     let ganador = false;
-
     for (let contadorColumna = 0; contadorColumna < matrizJuego.length; contadorColumna++) {
-
         if (matrizJuego[contadorFila][contadorColumna].jugador === jugador) {
             ganador = true;
         } else {
             ganador = false;
             break;
         }
-
         contadorFila -= 1;
     }
     return ganador;
@@ -61,13 +46,9 @@ function validarDiagonalB(jugador) {
 
 //Funcion para validar columna
 function validarColumna(columna, jugador) {
-
     let ganador = false;
-
     for (let indiceFila = 0; indiceFila < matrizJuego.length; indiceFila++) {
-
         let casilla = matrizJuego[indiceFila][columna] //10
-
         if (casilla.jugador === jugador) {
             ganador = true;
         } else {
@@ -80,30 +61,21 @@ function validarColumna(columna, jugador) {
 
 //Funcion para validar fila
 function validarFila(fila, jugador) {
-
     let ganador = false;
-
     let listaFila = matrizJuego[fila]
-
     for (let indiceColumna = 0; indiceColumna < listaFila.length; indiceColumna++) {
-
         let casilla = listaFila[indiceColumna] //10
-
         if (casilla.jugador === jugador) {
             ganador = true;
         } else {
             ganador = false;
             break;
         }
-
     }
     return ganador;
-
 }
 
-
 function actualizarTablero(idBoton) {
-
     for (let indiceFila = 0; indiceFila < matrizJuego.length; indiceFila++) {
         let fila = matrizJuego[indiceFila];
 
@@ -112,28 +84,37 @@ function actualizarTablero(idBoton) {
 
                 matrizJuego[indiceFila][indiceColumna].jugador = jugadorActual;
 
+                //desactivar boton
+                var botonPlayer = document.getElementById(idBoton)
+                botonPlayer.disabled = true;
+
+                //restar en 1 los turnos
+                turnosRestantes -= 1;
+
+
                 let ganador =
                     validarColumna(indiceColumna, jugadorActual) ||
                     validarFila(indiceFila, jugadorActual) ||
                     validarDiagonalA(jugadorActual) ||
                     validarDiagonalB(jugadorActual);
-
-
                 if (ganador === true) {
-
+                    ganadorJuego = true;
                     alert("EL JUGADOR " + jugadorActual + " HA GANADO");
                 }
-
                 break;
             }
 
         }
     }
+
 }
 
 
-
 function turnoComputadora() {
+    if (turnosRestantes === 0 || ganadorJuego) {
+        return;
+    }
+
     var listaCasillasLibres = [];
     for (let indiceFila = 0; indiceFila < matrizJuego.length; indiceFila++) {
         const fila = matrizJuego[indiceFila];
@@ -154,26 +135,10 @@ function turnoComputadora() {
     var botonCompu = document.getElementById(idCasillaComputadora)
     botonCompu.textContent = jugadorActual;
 
+
     jugadorActual = jugadorActual == "X" ? "O" : "X";
 
-
 }
-
-// actualizarTablero("celda-00");
-// jugadorActual = "O";
-// actualizarTablero("celda-11");
-// jugadorActual = "X";
-// actualizarTablero("celda-22");
-// jugadorActual = "O";
-// actualizarTablero("celda-20");
-// jugadorActual = "X";
-// actualizarTablero("celda-02");
-// jugadorActual = "O";
-// actualizarTablero("celda-01");
-// jugadorActual = "X";
-// actualizarTablero("celda-12");
-// // jugadorActual = "O";
-// // actualizarTablero("celda-21");
 
 // 4. Lógica principal
 
@@ -186,6 +151,9 @@ var listaEtiquetasBoton = document.querySelectorAll(".btn")
 for (let indiceListaBoton = 0; indiceListaBoton < listaEtiquetasBoton.length; indiceListaBoton++) {
     let boton = listaEtiquetasBoton[indiceListaBoton];
     boton.addEventListener("click", () => {
+        if (ganadorJuego) {
+            return;
+        }
         actualizarTablero(boton.id);
         boton.textContent = jugadorActual;
 
@@ -201,26 +169,3 @@ for (let indiceListaBoton = 0; indiceListaBoton < listaEtiquetasBoton.length; in
 // No se requiere ejecución inicial
 
 
-
-// function (celda){
-// for( indicefila ){
-
-//     for( indiceColuma ){
-
-//         if (   matris[indicefila][indiceColuma].celda ==== celda ){
-//             matris[indicefila][indiceColuma].jugador = jugadorActual;
-//             break
-//         }
-
-
-//     }
-// }
-
-
-
-var caja1 = document.createElement("img");
-caja1.setAttribute("src", "Clases/ProyectoGato/src/js/crash.png");
-var caja2 = document.createElement("img");
-caja2.setAttribute("src", "Clases/ProyectoGato/src/js/crash.png");
-
-console.log("Son iguales ", caja1.innerHTML == caja2.innerHTML)
